2ea377f61438a2d410cf661615b3c6c42e41e7df
Szőcs Gábor <szocsgabor@turr.hu>
2026-01-08 11:35:16 +0100

    bugfix:connectionStringAdd project files.

---
+++ AdatbazisTeszt/Database/DatabaseService.cs
@@ -0,0 +1,39 @@
+﻿using MySqlConnector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdatbazisTeszt.Database
+{
+    internal class DatabaseService
+    {
+        private static string connectionString;
+        private static string table;
+        private static string query_parameters;
+
+        public static void DbConnectionCheck(string connectionString)
+        {
+            try
+            {
+                using (MySqlConnection connection = new MySqlConnection(connectionString))
+                {
+                    connection.Open();
+                    Console.WriteLine("Sikeres a csatlakozás!!");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Sikertelen kapcsolódás");
+                Console.WriteLine(ex);
+            }
+
+        }
+
+
+
+
+
+    }
+}
---
+++ AdatbazisTeszt/Program.cs
@@ -0,0 +1,20 @@
+﻿using AdatbazisTeszt.Database;
+using System.Security.Cryptography.X509Certificates;
+
+internal class Program
+{
+
+    public static readonly string connectionString = "Server=localhost;Database=foldrajz;User=root;";
+   
+
+    private static void Main(string[] args)
+    {
+        //Console.WriteLine(connectionString);
+        DBCheck(connectionString);
+    }
+
+    private static void DBCheck(string connectionString)
+    {
+         DatabaseService.DbConnectionCheck(connectionString);
+    }
+}
