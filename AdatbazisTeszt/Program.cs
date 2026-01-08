a032673f632699a05f0f7fa663f31ce39174c260
Szőcs Gábor <szocsgabor@turr.hu>
2026-01-08 18:44:19 +0100

    fix.cS

--- AdatbazisTeszt/Program.cs
+++ AdatbazisTeszt/Program.cs
@@ -3,18 +3,10 @@ using System.Security.Cryptography.X509Certificates;
 
 internal class Program
 {
-
-
     public static readonly string connectionString = "Server=localhost;Database=11a_foldrajz;User=root;";
-  
-
-    public static readonly string connectionString = "Server=localhost;Database=11a_foldrajz;User:Root;";
->>>>>>> 971a7774c508bd5c7d70cf5fedd6024bd62f327b
-
 
     private static void Main(string[] args)
     {
->>>>>>>>> Temporary merge branch 2
         DBCheck(connectionString);
     }
 
